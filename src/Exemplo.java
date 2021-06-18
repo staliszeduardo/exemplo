2d379a33db11d15b651506cd450f0a4a315d8ff9
zLiTa <zLiTa@DESKTOP-HUG9OGT>
2021-06-17 22:48:26 -0300

    Adicionado o metodo SayHellowManyTimes

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,11 +1,16 @@
 
 public class Exemplo {
+	public static void sayHelloManyTimes(int times) {
+		for(int i = 0; i < times; i++) {
+			sayHello();
+		}
+	}
 	public static void sayHello() {
 		System.out.println("Ola Mundo");
 	}
 
 	public static void main(String[] args) {
-		sayHello();
+		sayHelloManyTimes(5);
 	}
 
 }
