9ccf2939162c05ee22d51d664572eceb7e4112ad
zLiTa <zLiTa@DESKTOP-HUG9OGT>
2021-06-17 22:42:22 -0300

    Incluido o metodo main e incluido o sayHello

--- src/Exemplo.java
+++ src/Exemplo.java
@@ -1,8 +1,11 @@
 
 public class Exemplo {
+	public static void sayHello() {
+		System.out.println("Ola Mundo");
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Ola Mundo");
+		sayHello();
 	}
 
 }
